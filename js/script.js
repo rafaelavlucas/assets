777cb35ae9c7500a4b91e93ab2b7ab8133b35e98
Rafaela <rafaelalucas_91@hotmail.com>
2019-08-09 16:38:21 +0100

    upd: css + js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 // tabs
 window.onload = function (e) {
 
-
+    // Show and Hide Icons
     var btnColor = document.querySelectorAll(".btn"),
-        icons = document.querySelectorAll(".icons");
-
+        icons = document.querySelectorAll(".icons"),
+        input = document.querySelector('.copyUrl');
 
     btnColor.forEach(function (el) {
         el.addEventListener("click", showColor);
     });
-
 
     function showColor(el) {
         var currentColor = el.currentTarget,
@@ -32,4 +31,38 @@ window.onload = function (e) {
 
     }
 
+    // Populate icon image
+
+    var iconTotal = 243;
+
+    function addIcons(color, iconNumber) {
+        var icon = document.getElementById(color);
+        for (let i = 1; i <= iconNumber; i++) {
+            var template = `<i><img src="icons/${color}/icon-${i}.svg"></i>`;
+            icon.insertAdjacentHTML('beforeend', template);
+        }
+    }
+
+    addIcons("white", iconTotal)
+    addIcons("black", iconTotal)
+
+    var icon = document.querySelectorAll('i img'),
+        copied = document.querySelector('.copied');
+
+    icon.forEach(function (element) {
+        element.addEventListener('click', selectSrc);
+    });
+
+    function selectSrc(event) {
+        var imgSrc = event.currentTarget.src;
+        input.value = imgSrc;
+        input.select();
+        document.execCommand('copy');
+
+        copied.classList.add('pop');
+
+        setTimeout(function () {
+            copied.classList.remove('pop');
+        }, 1000);
+    }
 }
